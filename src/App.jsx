15bf1fb66a229efee7183963f2d165b5607dad21
innazh <innazh1133@gmail.com>
2025-07-05 16:47:00 -0400

    add shadcn components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,11 +24,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Button variant="destructive">Button</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>The current time is {new Date(currentTime * 1000).toLocaleString()}.</p>
+        <p className='mt-4 font-bold'>The current time is {new Date(currentTime * 1000).toLocaleString()}.</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
