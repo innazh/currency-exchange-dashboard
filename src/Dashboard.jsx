cdcf32f5eb99619841c21b11f5e70c7708c43452
innazh <innazh1133@gmail.com>
2025-07-06 00:49:49 -0400

    dashboard draft

---
+++ src/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HistoricalExchangeChart from './Chart';
+// import CurrencyTable from './CurrencyTable';
+
+const Dashboard = () => {
+return (<>
+<HistoricalExchangeChart/>
+</>)
+};
+
+export default Dashboard;
