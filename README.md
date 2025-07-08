1eace70b2b7cce3a41687f4a2d6b80a81606e190
innazh <innazh1133@gmail.com>
2025-07-08 16:29:46 -0400

    persist table settings

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ If you are developing a production application, we recommend using TypeScript wi
 
 ## To run:
 You can open two terminal windows and run npm run dev in one to start the front end, and npm run api in the other to start the back end.
+
+Deploy:
+https://www.youtube.com/watch?v=qEfduVAQ8FQ
+
+!!!Persist grid settings
+on refresh!!!
+
+Optional Features – Not Required
+• Implement functionality to save API call data to a database for future use. So, an
+API call is not required to use the Frankfurter API each time.
+• Setup a function– this doesn’t have to be functional but in theory would run on the
+first of each month and add the new exchange rate data for the previous month.
+• Any additional features or improvements you believe would enhance the dashboard
+functionality or user experience.
--- src/CurrencyTable.jsx
+++ src/CurrencyTable.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
+import { BrushCleaning, Save } from 'lucide-react';
+import { toast } from "sonner"
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Toaster } from "@/components/ui/sonner"
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 const CurrencyTable = ({ data, pairs }) => {
+  const tableRef = useRef();
   const [rowData, setRowData] = useState([]);
 
   const [colDefs] = useState([
     { field: 'currency', headerName: 'Currency Pair', sortable: true, filter: true },
     { field: 'date', sortable: true, filter: true, },
-    { field: 'rate', sortable: true, filter: true },
+    {
+      field: 'rate', sortable: true, filter: true, valueFormatter: (params) => {
+        return params.value ? Number(params.value).toFixed(4) : '-';
+      },
+    },
   ]);
 
   const defaultColDef = {
     flex: 1,
     resizable: true,
+    sortable: true,
+    filter: true,
+    floatingFilter: true,
   };
 
   useEffect(() => {
@@ -33,20 +47,101 @@ const CurrencyTable = ({ data, pairs }) => {
     }
   }, [data, pairs]);
 
+  // Load table settings if they exist in local storage
+  const onGridReady = () => {
+    const settings = localStorage.getItem('currencyTableSettings');
+
+    if (!settings) {
+      return;
+    }
+
+    const parsedSettings = JSON.parse(settings);
+
+    if (parsedSettings.columns) {
+      tableRef.current.api.applyColumnState({
+        state: parsedSettings.columns,
+        applyOrder: true
+      });
+    }
+    if (parsedSettings.filters) {
+      tableRef.current.api.setFilterModel(parsedSettings.filters);
+    }
+    if (parsedSettings.sort && parsedSettings.sort.length > 0) {
+      tableRef.current.api.applyColumnState({
+        state: parsedSettings.sort,
+        defaultState: { sort: null }
+      });
+    }
+  }
+
+  const saveTableSettings = () => {
+    const columnState = tableRef.current.api.getColumnState();
+    const filterModel = tableRef.current.api.getFilterModel();
+    const paginationSize = tableRef.current.api.paginationGetPageSize();
+    const sortState = columnState
+      .filter(s => s.sort != null)
+      .map(s => ({
+        colId: s.colId,
+        sort: s.sort,
+        sortIndex: s.sortIndex
+      }));
+
+    const gridState = {
+      columns: columnState,
+      filters: filterModel,
+      sort: sortState,
+      pagination: {
+        pageSize: paginationSize,
+      }
+    };
+
+    localStorage.setItem('currencyTableSettings', JSON.stringify(gridState));
+    toast.success('Table settings have been saved');
+  }
+
+  const clearTableSettings = () => {
+    localStorage.removeItem('currencyTableSettings');
+    toast.info('Table settings cleared');
+  }
+
   return (
     <>
       {data && (
-        <AgGridReact className='flex-1'
-          rowData={rowData}
-          columnDefs={colDefs}
-          defaultColDef={defaultColDef}
-          domLayout="autoHeight"
-          animateRows
-          pagination={true}
-          paginationPageSize={20}
-        />
+        <Card className='w-full'>
+          <CardHeader className='gap-0'>
+            <div className='flex justify-end items-center gap-2'>
+              <Button variant='outline' size='sm' onClick={clearTableSettings}>
+                <BrushCleaning />
+                Table Settings
+              </Button>
+              <Button size='sm' onClick={saveTableSettings}>
+                <Save />
+                Table Settings
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent className='pt-0'>
+            <AgGridReact
+              ref={tableRef}
+              className='flex-1'
+              rowData={rowData}
+              columnDefs={colDefs}
+              defaultColDef={defaultColDef}
+              domLayout="autoHeight"
+              animateRows
+              pagination={true}
+              paginationPageSize={20}
+              enableCellTextSelection={true}
+              onGridReady={onGridReady} // load the settings from local storage
+              onRowClicked={() => {
+                // I want to add historical conversion functionality here
+              }}
+            />
+          </CardContent>
+        </Card>
         // https://www.ag-grid.com/react-data-grid/server-side-model-pagination/
       )}
+      <Toaster richColors />
     </>
   );
 };
