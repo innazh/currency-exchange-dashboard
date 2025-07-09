b35c6fe99283f03201253bcc927175bce1b3dbfc
innazh <innazh1133@gmail.com>
2025-07-09 14:29:22 -0400

    lib refactor

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -12,3 +12,12 @@ export const CURRENCY_PAIRS = [
   "USD/CAD",
   "USD/EUR",
 ];
+
+export const TIME_PERIODS = {
+  '7 days': 7,
+  '1 month': 1,
+  '3 months': 3,
+  '6 months': 6,
+  '1 year': 1,
+  '2 years': 2,
+};
--- src/lib/dates.js
+++ src/lib/dates.js
@@ -1,24 +1,40 @@
-export const addDays = (date, days) => {
+import {TIME_PERIODS} from './constants';
+
+const addDays = (date, days) => {
     const result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
 };
 
-export const subMonths = (date, months) => {
+const subMonths = (date, months) => {
     const result = new Date(date);
     result.setMonth(result.getMonth() - months);
     return result;
 };
 
-export const subYears = (date, years) => {
+const subYears = (date, years) => {
     const result = new Date(date);
     result.setFullYear(result.getFullYear() - years);
     return result;
 };
 
-export const formatDate = (date) => {
+const formatDate = (date) => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 };
+
+export const calculateFromDate = (reportingPeriod) => {
+    const today = new Date();
+    let startDate;
+
+    if (reportingPeriod.includes('day')) {
+    startDate = addDays(today, -TIME_PERIODS[reportingPeriod]);
+    } else if (reportingPeriod.includes('month')) {
+    startDate = subMonths(today, TIME_PERIODS[reportingPeriod]);
+    } else if (reportingPeriod.includes('year')) {
+    startDate = subYears(today, TIME_PERIODS[reportingPeriod]);
+    }
+    return formatDate(startDate);
+}
