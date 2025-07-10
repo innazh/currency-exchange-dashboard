d58561858ee0163d4941d08e84b75414b0e2adb6
innazh <innazh1133@gmail.com>
2025-07-10 15:19:07 -0400

    new linter, handle except

--- api/server.py
+++ api/server.py
@@ -2,13 +2,14 @@ from flask import Flask, request, jsonify, send_from_directory
 from db import init_db, close_db
 from exchange_rates import validate_request_args, fetch_frankfurter_data
 
-app = Flask(__name__,  static_folder='../dist', static_url_path='/')
+app = Flask(__name__, static_folder="../dist", static_url_path="/")
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory(app.static_folder, "index.html")
 
-@app.route('/api/rates', methods=['GET'])
+@app.route("/api/rates", methods=["GET"])
 def get_exchange_rates():
     is_valid, error, data = validate_request_args(request.args)
     if not is_valid:
@@ -16,21 +17,22 @@ def get_exchange_rates():
 
     from_date = data["from_date"].isoformat()
     to_date = data["to_date"].isoformat()
-    
-    results = {}
-    for pair in data["pairs"]:
-        key, value = fetch_frankfurter_data(pair, from_date, to_date)
-        results[key] = value
 
-    return jsonify({
-        "from": from_date,
-        "to": to_date,
-        "data": results
-    }), 200
+    try:
+        results = {}
+        for pair in data["pairs"]:
+            key, value = fetch_frankfurter_data(pair, from_date, to_date)
+            results[key] = value
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    return jsonify({"from": from_date, "to": to_date, "data": results}), 200
+
 
 @app.teardown_appcontext
 def teardown(exception):
     close_db()
 
+
 with app.app_context():
-    init_db() # this will only create the db if it doesn't exist
+    init_db()  # this will only create the db if it doesn't exist
